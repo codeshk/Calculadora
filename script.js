de76b160d3aad516179f0b68f0c943a6fe80869a
Gabriel Shiki <gabriel.mshiki@gmail.com>
2025-04-02 20:13:56 -0300

    Declaração de vars + primeiras functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function updateResult(originClear = false) {
+   result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+buttons.forEach((button) => {
+   button.addEventListener("click", () => {
+      const buttonText = button.innerText;
+      if (/^[0-9,]+$/.test(buttonText)) {
+         addDigit(buttonText);
+      }
+   });
+})
+
